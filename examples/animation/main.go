b8378f08394a272482b8ba06dd7558db764143da
post-l <ludovic.post@epitech.eu>
2018-06-05 11:09:48 -0700

    examples/animation: add multiple circles with diff color, size and speed

--- examples/animation/main.go
+++ examples/animation/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"image"
 	"image/color"
+	"math/rand"
 	"time"
 
 	"github.com/fogleman/gg"
@@ -20,7 +21,7 @@ var (
 
 func main() {
 	flag.Parse()
-
+	rand.Seed(time.Now().UnixNano())
 	if *emFlag {
 		m := emulator.NewEmulator(&matrix.DefaultHardwareConfig)
 		go func() {
@@ -51,45 +52,111 @@ func fatal(err error) {
 }
 
 type Animation struct {
-	ctx      *gg.Context
-	position image.Point
-	dir      image.Point
-	stroke   int
+	ctx     *gg.Context
+	circles []*Circle
 }
 
 func NewAnimation(sz image.Point) *Animation {
+	ctx := gg.NewContext(sz.X, sz.Y)
+	circles := make([]*Circle, 15)
+	for i := range circles {
+		circles[i] = NewCircle(ctx)
+	}
 	return &Animation{
-		ctx:    gg.NewContext(sz.X, sz.Y),
-		dir:    image.Point{1, 1},
-		stroke: 5,
+		ctx:     ctx,
+		circles: circles,
 	}
 }
 
 func (a *Animation) Image() image.Image {
 	a.ctx.SetColor(color.Black)
 	a.ctx.Clear()
-	a.ctx.DrawCircle(float64(a.position.X), float64(a.position.Y), float64(a.stroke))
-	a.ctx.SetColor(color.RGBA{255, 0, 0, 255})
-	a.ctx.Fill()
+	for _, c := range a.circles {
+		c.Draw()
+	}
 	return a.ctx.Image()
 }
 
 func (a *Animation) Delay() time.Duration {
-	return time.Second / 30
+	return time.Second / 10
 }
 
 func (a *Animation) Next() error {
-	a.position.X += 1 * a.dir.X
-	a.position.Y += 1 * a.dir.Y
-	if a.position.Y+a.stroke >= a.ctx.Height() {
-		a.dir.Y = -1
-	} else if a.position.Y-a.stroke < 0 {
-		a.dir.Y = 1
-	}
-	if a.position.X+a.stroke > a.ctx.Width() {
-		a.dir.X = -1
-	} else if a.position.X-a.stroke < 0 {
-		a.dir.X = 1
+	for _, c := range a.circles {
+		c.Next()
 	}
 	return nil
+}
+
+type Circle struct {
+	ctx      *gg.Context
+	position Point
+	dir      Point
+	color    color.RGBA
+	stroke   float64
+}
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func NewCircle(ctx *gg.Context) *Circle {
+	c := &Circle{
+		ctx:    ctx,
+		stroke: float64(rand.Intn(5) + 5),
+	}
+	c.position.X = float64(rand.Intn(ctx.Width()))
+	c.position.Y = float64(rand.Intn(ctx.Height()))
+	c.dir.X = initRandDir()
+	c.dir.Y = initRandDir()
+	c.randColor()
+	return c
+}
+
+func (c *Circle) Next() {
+	c.position.X += c.dir.X
+	c.position.Y += c.dir.Y
+	update := false
+	if c.position.Y+c.stroke >= float64(c.ctx.Height()) {
+		update = true
+		c.dir.Y = -randDir()
+	} else if c.position.Y-c.stroke < 0 {
+		update = true
+		c.dir.Y = randDir()
+	}
+	if c.position.X+c.stroke > float64(c.ctx.Width()) {
+		update = true
+		c.dir.X = -randDir()
+	} else if c.position.X-c.stroke < 0 {
+		update = true
+		c.dir.X = randDir()
+	}
+	if update {
+		c.randColor()
+	}
+}
+
+func (c *Circle) randColor() {
+	c.color.R = uint8(rand.Intn(256))
+	c.color.G = uint8(rand.Intn(256))
+	c.color.B = uint8(rand.Intn(256))
+}
+
+func (c *Circle) Draw() {
+	c.ctx.DrawCircle(c.position.X, c.position.Y, c.stroke)
+	c.ctx.SetColor(c.color)
+	c.ctx.Fill()
+}
+
+func initRandDir() float64 {
+	dir := rand.Float64()*2 - 1
+	if dir < 0 {
+		return dir - 1
+	}
+	return dir + 1
+}
+
+func randDir() float64 {
+	return (rand.Float64() / 2) + 2
 }
