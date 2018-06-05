fdc7dfaabf50a3f34c4adbf359ee6212e5751dfa
post-l <ludovic.post@epitech.eu>
2018-06-05 11:15:19 -0700

    examples/life: game of life with colors

--- examples/life/main.go
+++ examples/life/main.go
@@ -2,12 +2,12 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"flag"
 	"image"
 	"image/color"
 	"log"
+	"math"
 	"math/rand"
 	"time"
 
@@ -54,35 +54,22 @@ func fatal(err error) {
 }
 
 type Animation struct {
-	l   *Life
-	img *image.RGBA
+	l *Life
 }
 
 func NewAnimation(r image.Rectangle) *Animation {
 	sz := r.Size()
 	return &Animation{
-		l:   NewLife(sz.X, sz.Y),
-		img: image.NewRGBA(r),
+		l: NewLife(sz.X, sz.Y),
 	}
 }
 
 func (*Animation) Delay() time.Duration {
-	return time.Second / 20
+	return time.Second / 10
 }
 
 func (a *Animation) Image() image.Image {
-	for y := 0; y < a.l.h; y++ {
-		for x := 0; x < a.l.w; x++ {
-			var c color.Color
-			if a.l.a.Alive(x, y) {
-				c = color.RGBA{255, 0, 0, 255}
-			} else {
-				c = color.Black
-			}
-			a.img.Set(x, y, c)
-		}
-	}
-	return a.img
+	return a.l.a
 }
 
 func (a *Animation) Next() error {
@@ -92,28 +79,38 @@ func (a *Animation) Next() error {
 
 // Field represents a two-dimensional field of cells.
 type Field struct {
-	s    [][]bool
+	s    [][]color.RGBA
 	w, h int
 }
 
 // NewField returns an empty field of the specified width and height.
 func NewField(w, h int) *Field {
-	s := make([][]bool, h)
+	s := make([][]color.RGBA, h)
 	for i := range s {
-		s[i] = make([]bool, w)
+		s[i] = make([]color.RGBA, w)
 	}
 	return &Field{s: s, w: w, h: h}
 }
 
-// Set sets the state of the specified cell to the given value.
-func (f *Field) Set(x, y int, b bool) {
-	f.s[y][x] = b
+// ColorModel returns the canvas' color model, always color.RGBAModel
+func (*Field) ColorModel() color.Model { return color.RGBAModel }
+
+// Bounds return the topology of the Canvas
+func (f *Field) Bounds() image.Rectangle { return image.Rect(0, 0, f.w, f.h) }
+
+func (f *Field) At(x, y int) color.Color {
+	return f.s[y][x]
 }
 
-// Alive reports whether the specified cell is alive.
+// Set sets the state of the specified cell to the given value.
+func (f *Field) Set(x, y int, c color.RGBA) {
+	f.s[y][x] = c
+}
+
+// Get returns the specified cell.
 // If the x or y coordinates are outside the field boundaries they are wrapped
 // toroidally. For instance, an x value of -1 is treated as width-1.
-func (f *Field) Alive(x, y int) bool {
+func (f *Field) Get(x, y int) color.RGBA {
 	x += f.w
 	x %= f.w
 	y += f.h
@@ -122,13 +119,20 @@ func (f *Field) Alive(x, y int) bool {
 }
 
 // Next returns the state of the specified cell at the next time step.
-func (f *Field) Next(x, y int) bool {
+func (f *Field) Next(x, y int) color.RGBA {
 	// Count the adjacent cells that are alive.
 	alive := 0
+	var r, g, b uint32
 	for i := -1; i <= 1; i++ {
 		for j := -1; j <= 1; j++ {
-			if (j != 0 || i != 0) && f.Alive(x+i, y+j) {
-				alive++
+			if j != 0 || i != 0 {
+				c := f.Get(x+i, y+j)
+				if c != (color.RGBA{}) {
+					alive++
+					r += uint32(c.R)
+					g += uint32(c.G)
+					b += uint32(c.B)
+				}
 			}
 		}
 	}
@@ -136,7 +140,17 @@ func (f *Field) Next(x, y int) bool {
 	//   exactly 3 neighbors: on,
 	//   exactly 2 neighbors: maintain current state,
 	//   otherwise: off.
-	return alive == 3 || alive == 2 && f.Alive(x, y)
+	if alive == 3 {
+		return color.RGBA{
+			A: 255,
+			R: uint8(math.Min(float64(r)*0.3336666667, 255)),
+			G: uint8(math.Min(float64(g)*0.3336666667, 255)),
+			B: uint8(math.Min(float64(b)*0.3336666667, 255)),
+		}
+	} else if alive == 2 {
+		return f.s[y][x]
+	}
+	return color.RGBA{}
 }
 
 // Life stores the state of a round of Conway's Game of Life.
@@ -148,8 +162,16 @@ type Life struct {
 // NewLife returns a new Life game state with a random initial state.
 func NewLife(w, h int) *Life {
 	a := NewField(w, h)
-	for i := 0; i < (w * h / 4); i++ {
-		a.Set(rand.Intn(w), rand.Intn(h), true)
+	c := color.RGBA{A: 255, R: 231, G: 76, B: 60}
+	nbCells := w * h / 4
+	nbCCells := nbCells / 3
+	for i := 0; i < nbCells; i++ {
+		if i == nbCCells {
+			c = color.RGBA{A: 255, R: 46, G: 204, B: 113}
+		} else if i == nbCCells*2 {
+			c = color.RGBA{A: 255, R: 52, G: 152, B: 219}
+		}
+		a.Set(rand.Intn(w), rand.Intn(h), c)
 	}
 	return &Life{
 		a: a, b: NewField(w, h),
@@ -167,30 +189,4 @@ func (l *Life) Step() {
 	}
 	// Swap fields a and b.
 	l.a, l.b = l.b, l.a
-}
-
-// String returns the game board as a string.
-func (l *Life) String() string {
-	var buf bytes.Buffer
-	for x := 0; x < l.w+2; x++ {
-		buf.WriteByte('-')
-	}
-	buf.WriteByte('\n')
-	for y := 0; y < l.h; y++ {
-		buf.WriteByte('|')
-		for x := 0; x < l.w; x++ {
-			b := byte(' ')
-			if l.a.Alive(x, y) {
-				b = '*'
-			}
-			buf.WriteByte(b)
-		}
-		buf.WriteByte('|')
-		buf.WriteByte('\n')
-	}
-	for x := 0; x < l.w+2; x++ {
-		buf.WriteByte('-')
-	}
-	buf.WriteByte('\n')
-	return buf.String()
 }
